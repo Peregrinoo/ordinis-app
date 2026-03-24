fbe01a6bde41a1fc63dc37f58eca9367801f44b2
peregrinoo <gabriel.ribeiro07@aluno.ifce.edu.br>
2026-03-23 22:21:12 -0300

    fix: Adding logo and fixing a old dependencie

--- lib/screens/splash-screen/splash_screen.dart
+++ lib/screens/splash-screen/splash_screen.dart
@@ -1,41 +1,8 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ordinis/providers/daily_quote_provider.dart';
-import 'package:ordinis/services/quote_service.dart';
 import 'package:provider/provider.dart';
-
-void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => DailyQuoteProvider(
-            service: QuoteService(),
-          ),
-        ),
-      ],
-      child: const MinhaAplicacao(),
-    ),
-  );
-}
-
-class MinhaAplicacao extends StatelessWidget {
-  const MinhaAplicacao({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Ordinis - Santo Agostinho',
-      theme: ThemeData(
-        scaffoldBackgroundColor: const Color(0xFFEBE5D2),
-      ),
-      home: const SplashPage(),
-    );
-  }
-}
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
